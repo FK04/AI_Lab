55296119420f00f69d3e1ab3a9ce51907a49b2ba
SanaMateen <114050201+SanaMateen@users.noreply.github.com>
2023-09-19 19:02:23 +0530

    modified waterjug code

--- waterjug.py
+++ waterjug.py
@@ -1,9 +1,27 @@
+
+import math
+
 a=int(input("Enter Jug A Capacity: "))
 b=int(input("Enter Jug B Capacity: "))
 ai=int(input("Initially Water in Jug A: "))
 bi=int(input("Initially Water in Jug B: "))
 af=int(input("Final State of Jug A: "))
 bf=int(input("Final State of Jug B: "))
+
+gcd=math.gcd(a,b)
+print(gcd)
+if((af<=a+b) and af%gcd==0) or((bf<=a+b)and bf%gcd==0):
+	print("solution not possible")
+	exit(1)
+
+
+if af > a or bf > b or af + bf > a + b :
+    print("The final state is not achievable with the given capacities.")
+    exit(1) 
+elif (af > a and bf > a) or (af > b and bf > b):
+    print("The final state is not achievable with the given capacities.")
+    exit(1)
+
 print("List Of Operations You can Do:\n")
 print("1.Fill Jug A Completely\n")
 print("2.Fill Jug B Completely\n")
@@ -13,6 +31,9 @@ print("5.Pour From Jug A till Jug B filled Completely or A becomes empty\n")
 print("6.Pour From Jug B till Jug A filled Completely or B becomes empty\n")
 print("7.Pour all From Jug B to Jug A\n")
 print("8.Pour all From Jug A to Jug B\n")
+
+
+
 while ((ai!=af or bi!=bf)):
 	op=int(input("Enter the Operation: "))
 	if(op==1):
@@ -36,11 +57,18 @@ while ((ai!=af or bi!=bf)):
 			bi=0
 		else:
 			bi=bi-(a-ai)
+			print("***",ai)
 			ai=a
 	elif(op==7):
-		ai=ai+bi
-		bi=0
+		if(ai<a):
+			ai=ai+bi
+			bi=0
+		else:
+			print("jug A overflow cant proceed")
 	elif(op==8):
-		bi=bi+ai
-		ai=0
+		if(bi<b):
+			bi=bi+ai
+			ai=0
+		else:
+			print("jug b overflow cant proceed")
 	print(ai,bi)
