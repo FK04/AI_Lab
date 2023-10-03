fef2b304da425d9a577063aeecd0d37a81d50baa
SanaMateen <114050201+SanaMateen@users.noreply.github.com>
2023-10-03 18:39:31 +0530

    modified version

--- hangmanv5.py
+++ hangmanv5.py
@@ -3,38 +3,43 @@ import random
 from hangman_words import word_list
 from hangman_art import stages,logo
 chosen_word=random.choice(word_list)
-lives=6
+lives=6 if len(chosen_word) >= 6 else 6
 #testing code
 print(f'Pssst, the solution is {chosen_word}.')
 print(logo)
+print(len(stages))
 display=[]
-
+guessed=[]
 for i in range(len(chosen_word)):
    
 	display.append("_")
 while "_" in display:
 	guess=input("Guess a letter:").lower()
-	for i in range(len(chosen_word)):
- 
-		letter=chosen_word[i]
-		 
-		if letter==guess:
-    
+	if len(guess)==1 and guess.isalpha():
+		if guess in guessed:
+			print("letter already present")
+			continue
+		guessed.append(guess)  
+		for i in range(len(chosen_word)):
+	
+			letter=chosen_word[i]
 			
-			display[i]=letter  
-		
-	if guess not in chosen_word:
-            
-		lives=lives-1
-            
-		
-		if lives==0:
-            
-			print("You loose")
-            
-			exit(1)
-	print(f"{' '.join(display)}")
-	print(stages[lives])
+			if letter==guess:
+				display[i]=letter	
+		if guess not in chosen_word:
+			if lives > 0:	
+				lives=lives-1
+				if lives==0:
+					print("You loose")
+					print(stages[lives])
+					print(f'the solution is {chosen_word}.')
+								
+					exit(1)
+		print(f"{' '.join(display)}")
+		print(lives)
+		print(stages[lives])
+	else:
+		print("guess should be a character rather than a word")
 		
 else:
 	print("You have won")
